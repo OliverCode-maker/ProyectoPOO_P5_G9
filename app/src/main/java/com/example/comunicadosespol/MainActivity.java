d408483072e1c8956110ca7476dfa3209963c68c
pablo ojeda <pablojed@espol.edu.ec>
2025-08-09 10:07:47 -0500

    Cambios realizados en el MainActivity y txt:
    - Separación de lógica de autenticación en el método autenticar:
    En este método se verificar si el usuario con la contraseña dados existen están registrados
    - Modificación del método iniciarSesion acorde al nuevo método autenticar
    Se reduce líneas al poner parte de su lógica en el método autenticar
    - Manejo de errores más robusto para el método cargarUsuarios
    Se agrega try al cargar los datos, antes de darles formato
    - Se agregó también comunicados.txt para poder llenarse después en assets

--- app/src/main/java/com/example/comunicadosespol/MainActivity.java
+++ app/src/main/java/com/example/comunicadosespol/MainActivity.java
@@ -44,41 +44,61 @@ public class MainActivity extends AppCompatActivity {
 
         cargarUsuarios();
     }
-    public void iniciarSesion(View view){
-        String name= editUser.getText().toString().trim();
-        String psw = editPass.getText().toString().trim();
+    // Lógica de autenticación
+    private Usuario autenticar(String username, String password) throws CredencialesInvalidasException {
+        for (Usuario u : usuarios) {
+            if (u.getUsername().equals(username) && u.getPassword().equals(password)) {
+                return u; // éxito
+            }
+        }
+        // si no se encontró, lanzamos la excepción verificada
+        throw new CredencialesInvalidasException("Usuario o contraseña incorrecta");
+    }
 
-        if(name.isEmpty()||psw.isEmpty()){
+    public void iniciarSesion(View view){
+        String username= editUser.getText().toString().trim();
+        String password = editPass.getText().toString().trim();
+
+        if(username.isEmpty()||password.isEmpty()){
             Toast.makeText(this,"Llene todos los campos",Toast.LENGTH_SHORT).show();
         }else{
             try {
-                for (Usuario u : usuarios) {
-                    if (u.getUsername().equals(name) && u.getPassword().equals(psw)) {
-                        Intent intent= new Intent(MainActivity.this,AdminComActivity.class);
-                        startActivity(intent);
-                    }
-                }
-                throw new CredencialesInvalidasException("Usuario o contraseña incorrecta");
-
-            }catch(CredencialesInvalidasException e){
+                Usuario u = autenticar(username, password); // puede lanzar la excepción verificada
+                Intent intent = new Intent(MainActivity.this, AdminComActivity.class);
+                //Agregar id del usuario para trackearlo en la siguiente vista
+                intent.putExtra("userId", u.getIdUser());
+                startActivity(intent);
+                finish();
+            } catch(CredencialesInvalidasException e){
                 Toast.makeText(this,e.getMessage(),Toast.LENGTH_SHORT).show();
             }
         }
     }
-
     public void cargarUsuarios(){
+        usuarios.clear(); // evita duplicados si onCreate se ejecuta otra vez
+        // verificación de errores al cargar usuarios
         try (InputStream is = getAssets().open("usuarios.txt");
              BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-
+            int i=1;
             String linea;
             while ((linea = reader.readLine()) != null) {
-                String[] info =linea.split(",");
-                usuarios.add(new Usuario(Integer.parseInt(info[0]),info[1],info[2]));
-            }
+                String[] info = linea.split(",");
+                if (info.length < 3) continue; // o reporta
+                // verificación de errores al darle formato a los datos
+                try {
+                    int id = Integer.parseInt(info[0].trim());
+                    String user = info[1].trim();
+                    String pass = info[2].trim();
+                    usuarios.add(new Usuario(id, user, pass));
+                } catch (NumberFormatException ex) {
+                    System.out.println("Error de formato en línea " +i+": " + linea);
+                }
 
+                i++;
+            }
         } catch (IOException e) {
             Toast.makeText(this,"Error al cargar usuarios...",Toast.LENGTH_SHORT).show();
         }
-
     }
+
 }
--- app/src/main/java/com/example/comunicadosespol/PublComunicadosActivity.java
+++ app/src/main/java/com/example/comunicadosespol/PublComunicadosActivity.java
@@ -1,15 +1,46 @@
 package com.example.comunicadosespol;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class PublComunicadosActivity extends AppCompatActivity {
+    private RadioGroup rtipos;
+    private Spinner spArea;
+    private CheckBox chkEst;
+    private CheckBox chkPrf;
+    private CheckBox chkAdm;
+    private EditText editTitle;
+    private EditText editDesc;
+    private Button btnImg;
+    private Button btnPublicar;
+    private Button btnCancelar;
+    private Uri imageUri;
+
+    private ImageView vistaImagen;
+    private static final int PICK_IMAGE = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +52,70 @@ public class PublComunicadosActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnImg = findViewById(R.id.btnCargarImagen);
+        btnPublicar = findViewById(R.id.btnPubl);
+        rtipos = findViewById(R.id.rdTipo);
+
+        btnImg.setOnClickListener(v -> openGallery());
+
+        btnPublicar.setOnClickListener(v -> {
+            if (imageUri != null) {
+                saveUriToFile(imageUri.toString());
+                Toast.makeText(this, "URI guardada", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Seleccione una imagen primero", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rtipos.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // 'checkedId' es el id del RadioButton seleccionado
+                if (checkedId != -1) { // -1 significa que no hay selección
+                    // Buscar el RadioButton por su id
+                    RadioButton radioSeleccionado = findViewById(checkedId);
+                    int posicion = group.indexOfChild(radioSeleccionado);
+
+                    // Ejemplo: si no es la primera opción
+                    if (posicion != 0) {
+                        mostrarCampo(posicion);
+                    }
+                }
+            }
+        });
+
+
+
     }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+            vistaImagen.setImageURI(imageUri);
+        }
+    }
+
+    private void saveUriToFile(String uriString) {
+        try {
+            FileOutputStream fos = openFileOutput("image_uri.txt", MODE_PRIVATE);
+            fos.write(uriString.getBytes());
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
     public void cargarImg(View view){
 
     }
@@ -30,5 +124,12 @@ public class PublComunicadosActivity extends AppCompatActivity {
     }
     public void cancelar(View view){
 
+    }
+
+    public void mostrarCampo(int posicion) {
+        switch (posicion) {
+            case 1:
+                
+        }
     }
 }
